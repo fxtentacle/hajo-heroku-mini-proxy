1a6edd701ee1d9e3baab7b834a06da9d44a8d735
Hajo Nils Krabbenhöft <fxtentacle@googlemail.com>
2020-08-17 14:30:03 +0200

    clients need to GET /proxyauth/[passwd] to be added to the IP whitelist

--- main.go
+++ main.go
@@ -13,7 +13,28 @@ func main() {
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.Verbose = true
 
+	allowed_ips := make(map[string]bool)
+
 	auth := func(writer http.ResponseWriter, request *http.Request) {
+		ip := request.Header.Get("X-FORWARDED-FOR")
+		if ip == "" { ip = strings.SplitN(request.RemoteAddr,":",2)[0] }
+
+		if request.URL.Path == "/proxyauth/"+os.Getenv("PROXYAUTH") {
+			fmt.Println("AUTHORIZE IP: ", ip)
+			allowed_ips[ip] = true
+			writer.WriteHeader(200)
+			writer.Write([]byte("200 OK PROXYAUTH\n"))
+			return
+		}
+
+		if !allowed_ips[ip] {
+			fmt.Println("IP NOT AUTHORIZED: ", ip)
+			writer.WriteHeader(500)
+			writer.Write([]byte("500 PROXYAUTH\n"))
+			return
+		}
+		fmt.Println("IP is authorized: ", ip)
+
 		proxy.ServeHTTP(writer, request)
 	}
 
